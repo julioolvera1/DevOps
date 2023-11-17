5ffff3db01dc83921c6723859d2a5d33b4b1a80c
Julio Perez Olvera <julioolvera1@gmail.com>
2023-11-17 14:00:01 +0000

    Added a new bash script and edited the Github action to run it

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "I am being run from a script"
+touch fileA.txt fileB.txt fileC.txt
+echo "Created fileA.txt fileB.txt fileC.txt"
+echo "Listing current directory contents"
+ls -al 
